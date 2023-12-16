f729df1869dfc46b40e69f39bf50c13c2209e1c6
firmnardians <4647804-firmnardians@users.noreply.gitlab.com>
2023-12-16 21:52:31 +0700

    fix: change unsigned type

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ fn main( )  {
     generate_fizzbuzz(100);
 }
 
-fn is_valid(num: i32, a: i32) -> bool {
+fn is_valid(num: u32, a: u32) -> bool {
     if a == 0 {
         return false
     }
@@ -10,7 +10,7 @@ fn is_valid(num: i32, a: i32) -> bool {
     num % a == 0
 }
 
-fn generate_fizzbuzz(value: i32) {
+fn generate_fizzbuzz(value: u32) {
     for i in 1..=value{
         if is_valid(i, 3) && is_valid(i, 5) {
             println!("Fizz Buzz")
