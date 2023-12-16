2d9de1185460336c6127843e8ab63b226541547e
firmnardians <4647804-firmnardians@users.noreply.gitlab.com>
2023-12-16 21:47:01 +0700

    feat: add function to check value

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 fn main( )  {
-    generate_fizzbuzz(10);
+    generate_fizzbuzz(100);
 }
 
+fn is_valid(num: i32, a: i32) -> bool {
+    if a == 0 {
+        return false
+    }
 
-fn generate_fizzbuzz(value: i32)  {
+    num % a == 0
+}
+
+fn generate_fizzbuzz(value: i32) {
     for i in 1..=value{
-
-        if i % 3 == 0 && i % 5 == 0 {
+        if is_valid(i, 3) && is_valid(i, 5) {
             println!("Fizz Buzz")
-        }else if i % 3 == 0 {
+        }else if is_valid(i, 3) {
             println!("Fizz")
-        }else if i % 5 == 0 {
+        }else if is_valid(i, 5) {
             println!("Buzz")
         }else {
             println!("{}", i)
