bad8de35898d32635c89c8a93ed72dc9ddcb2af1
firmnardians <4647804-firmnardians@users.noreply.gitlab.com>
2023-12-16 21:36:32 +0700

    fix: change increment value

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 fn main( )  {
-    generate_fizzbuzz(100);
+    generate_fizzbuzz(10);
 }
 
 
 fn generate_fizzbuzz(value: i32)  {
-    let value = value + 1;
-
-    for i in 1..value{
+    for i in 1..=value{
 
         if i % 3 == 0 && i % 5 == 0 {
             println!("Fizz Buzz")
@@ -17,6 +15,5 @@ fn generate_fizzbuzz(value: i32)  {
         }else {
             println!("{}", i)
         }
-
     }
 }
